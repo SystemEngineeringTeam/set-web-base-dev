39b065181a5cf0acfdd1754ce71dbb03a533fbba
k15015kk <k15015kk@MacBook-Pro-by-ericwifina.local>
2017-03-10 00:05:15 +0900

    opa changed

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@ $(function() {
 
     $('#area-myname').hover(
         function(e) {
-            $(this).css('background', 'rgba(255,255,255,0.8)');
+            $(this).css('background', 'rgba(255,255,255,0.9)');
             $('#title-text').css('color', 'rgb(255,87,34)');
         },
         function(e) {
-            $(this).css('background', 'rgba(255,87,34,0.8)');
+            $(this).css('background', 'rgba(255,87,34,0.9)');
             $('#title-text').css('color', 'rgb(255,255,255)');
         }
     );
@@ -16,62 +16,62 @@ $(function() {
         flag = 1;
         $('#page001').css('display', 'block');
         $('#page002').css('display', 'none');
-        $('.content-link').css('background', 'rgba(255,87,34,0.8)');
+        $('.content-link').css('background', 'rgba(255,87,34,0.9)');
         $('.content-link').children('p').css('color', 'rgb(255, 255, 255)');
     });
 
     $('#area-twitter').hover(
         function(e) {
-            $(this).css('background', 'rgba(255,255,255,0.8)');
+            $(this).css('background', 'rgba(255,255,255,0.9)');
             $('#header-twitter').css('color', 'rgb(29,161,242)');
         },
         function(e) {
-            $(this).css('background', 'rgba(29,161,242,0.8)');
+            $(this).css('background', 'rgba(29,161,242,0.9)');
             $('#header-twitter').css('color', 'rgb(255,255,255)');
         }
     );
 
     $('#area-twitter').on('click', function() {
         window.open('https://twitter.com/set_official');
-        $(this).css('background', 'rgba(29,161,242,0.8)');
+        $(this).css('background', 'rgba(29,161,242,0.9)');
         $('#header-twitter').css('color', 'rgb(255,255,255)');
     });
 
     $('#area-github').hover(
         function(e) {
-            $(this).css('background', 'rgba(255,255,255,0.8)');
+            $(this).css('background', 'rgba(255,255,255,0.9)');
             $('#header-github').css('color', 'rgb(36,41,46)');
         },
         function(e) {
-            $(this).css('background', 'rgba(36,41,46,0.8)');
+            $(this).css('background', 'rgba(36,41,46,0.9)');
             $('#header-github').css('color', 'rgb(255,255,255)');
         }
     );
 
     $('#area-github').on('click', function() {
         window.open('https://github.com/SystemEngineeringTeam');
-        $(this).css('background', 'rgba(36,41,46,0.8)');
+        $(this).css('background', 'rgba(36,41,46,0.9)');
         $('#header-github').css('color', 'rgb(255,255,255)');
     });
 
     $('#area-blog').hover(
         function(e) {
-            $(this).css('background', 'rgba(255,255,255,0.8)');
+            $(this).css('background', 'rgba(255,255,255,0.9)');
             $('#header-blog').css('color', 'rgb(175,180,43)');
         },
         function(e) {
-            $(this).css('background', 'rgba(175,180,43,0.8)');
+            $(this).css('background', 'rgba(175,180,43,0.9)');
             $('#header-blog').css('color', 'rgb(255,255,255)');
         }
     );
 
     $('#area-product').hover(
         function(e) {
-            $(this).css('background', 'rgba(255,255,255,0.8)');
+            $(this).css('background', 'rgba(255,255,255,0.9)');
             $('#header-product').css('color', 'rgb(0,121,107)');
         },
         function(e) {
-            $(this).css('background', 'rgba(0,121,107,0.8)');
+            $(this).css('background', 'rgba(0,121,107,0.9)');
             $('#header-product').css('color', 'rgb(255,255,255)');
         }
     );
@@ -98,15 +98,15 @@ $(function() {
 
     $('.content-link').hover(
         function(e) {
-            $(this).css('background', 'rgba(250,250,250,0.8)');
+            $(this).css('background', 'rgba(250,250,250,0.9)');
             $(this).children('p').css('color', 'rgb(255, 87, 34)');
         },
         function(e) {
-            $(this).css('background', 'rgba(255,87,34,0.8)');
+            $(this).css('background', 'rgba(255,87,34,0.9)');
             $(this).children('p').css('color', 'rgb(255, 255, 255)');
 
             if (flag == 2) {
-                $('#content-link-about').css('background', 'rgba(250,250,250,0.8)');
+                $('#content-link-about').css('background', 'rgba(250,250,250,0.9)');
                 $('#content-link-about').children('p').css('color', 'rgb(255, 87, 34)');
             }
         }
@@ -116,7 +116,7 @@ $(function() {
         flag = 2;
         $('#page001').css('display', 'none');
         $('#page002').css('display', 'block');
-        $(this).css('background', 'rgba(250,250,250,0.8)');
+        $(this).css('background', 'rgba(250,250,250,0.9)');
         $(this).children('p').css('color', 'rgb(255, 87, 34)');
     });
 });
