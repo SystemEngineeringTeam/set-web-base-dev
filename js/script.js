b4c316d524b6000c9fcf2e8305d450dc24bcf92d
k15015kk <k15015kk@MacBook-Pro-by-ericwifina.local>
2017-03-11 00:45:23 +0900

    minamike adds

--- js/script.js
+++ js/script.js
@@ -137,6 +137,10 @@ $(function() {
         $(this).css('background', 'rgba(250,250,250,0.9)');
         $(this).children('p').css('color', 'rgb(255, 87, 34)');
     });
+
+    $('#area-page003-title-sub').on('click', function () {
+        window.open('https://www.amazon.co.jp/%E3%81%BF%E3%81%AA%E3%81%BF%E3%81%91-%E3%82%B3%E3%83%9F%E3%83%83%E3%82%AF-1-14%E5%B7%BB%E3%82%BB%E3%83%83%E3%83%88-%E3%83%A4%E3%83%B3%E3%83%9E%E3%82%ACKC%E3%82%B9%E3%83%9A%E3%82%B7%E3%83%A3%E3%83%AB-%E3%82%B3%E3%83%8F%E3%83%AB/dp/B01EIWC8ZK/ref=sr_1_4?ie=UTF8&qid=1489122963&sr=8-4&keywords=%E3%81%BF%E3%81%AA%E3%81%BF%E3%81%91');
+    });
 });
 
 function contentLinkReset() {
