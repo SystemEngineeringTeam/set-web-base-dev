74cf1fedabca16de469cd228a4fd0bfeba8bf1c7
k15015kk <k15015kk@MacBook-Pro-by-ericwifina.local>
2017-04-10 18:02:04 +0900

    ver2.03

--- js/script.js
+++ js/script.js
@@ -16,7 +16,6 @@ $(function() {
         flag = 1;
         $('#page001').css('display', 'block');
         $('#page002').css('display', 'none');
-        $('#page003').css('display', 'none');
         $('#page004').css('display', 'none');
         $('#page005').css('display', 'none');
         window.scrollTo(0, 0);                
@@ -112,8 +111,7 @@ $(function() {
                 $('#content-link-about').css('background', 'rgba(250,250,250,0.9)');
                 $('#content-link-about').children('p').css('color', 'rgb(255, 87, 34)');
             } else if (flag == 3) {
-                $('#content-link-new').css('background', 'rgba(250,250,250,0.9)');
-                $('#content-link-new').children('p').css('color', 'rgb(255, 87, 34)');
+                console.log("error");
             } else if (flag == 4) {
                 $('#content-link-member').css('background', 'rgba(250,250,250,0.9)');
                 $('#content-link-member').children('p').css('color', 'rgb(255, 87, 34)');                
@@ -128,7 +126,6 @@ $(function() {
         flag = 2;
         $('#page001').css('display', 'none');
         $('#page002').css('display', 'block');
-        $('#page003').css('display', 'none');
         $('#page004').css('display', 'none');
         $('#page005').css('display', 'none');            
         window.scrollTo(0, 0);
@@ -137,24 +134,10 @@ $(function() {
         $(this).children('p').css('color', 'rgb(255, 87, 34)');
     });
 
-    $('#content-link-new').on('click', function () {
-        flag = 3;
-        $('#page001').css('display', 'none');
-        $('#page002').css('display', 'none');
-        $('#page003').css('display', 'block');
-        $('#page004').css('display', 'none');        
-        $('#page005').css('display', 'none');        
-        window.scrollTo(0, 0);        
-        contentLinkReset();        
-        $(this).css('background', 'rgba(250,250,250,0.9)');
-        $(this).children('p').css('color', 'rgb(255, 87, 34)');
-    });
-
     $('#content-link-member').on('click', function () {
         flag = 4;
         $('#page001').css('display', 'none');
         $('#page002').css('display', 'none');
-        $('#page003').css('display', 'none');
         $('#page004').css('display', 'block');
         $('#page005').css('display', 'none');                
         window.scrollTo(0, 0);        
@@ -167,17 +150,12 @@ $(function() {
         flag = 5;
         $('#page001').css('display', 'none');
         $('#page002').css('display', 'none');
-        $('#page003').css('display', 'none');
         $('#page004').css('display', 'none');
         $('#page005').css('display', 'block');                
         window.scrollTo(0, 0);        
         contentLinkReset();        
         $(this).css('background', 'rgba(250,250,250,0.9)');
         $(this).children('p').css('color', 'rgb(255, 87, 34)');
-    });
-
-    $('#area-page003-title-sub').on('click', function () {
-        window.open('https://www.amazon.co.jp/%E3%81%BF%E3%81%AA%E3%81%BF%E3%81%91-%E3%82%B3%E3%83%9F%E3%83%83%E3%82%AF-1-14%E5%B7%BB%E3%82%BB%E3%83%83%E3%83%88-%E3%83%A4%E3%83%B3%E3%83%9E%E3%82%ACKC%E3%82%B9%E3%83%9A%E3%82%B7%E3%83%A3%E3%83%AB-%E3%82%B3%E3%83%8F%E3%83%AB/dp/B01EIWC8ZK/ref=sr_1_4?ie=UTF8&qid=1489122963&sr=8-4&keywords=%E3%81%BF%E3%81%AA%E3%81%BF%E3%81%91');
     });
 
     $('#area-button-member').hover(
